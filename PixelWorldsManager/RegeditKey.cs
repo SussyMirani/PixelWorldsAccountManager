fee0746330c88482e07d697afb2e52c4cc54585d
MiraniGD <57767778+MiraniGD@users.noreply.github.com>
2023-06-22 14:21:44 +0300

    Update RegeditKey.cs
    Deleted nonsense code

--- PixelWorldsManager/RegeditKey.cs
+++ PixelWorldsManager/RegeditKey.cs
@@ -13,29 +13,25 @@ namespace PixelWorldsManager
 {
     internal class RegeditKey : MainWindow
     {
-        public (string Cognito, string LastLogin, object LoggedOut) CognitoAndLogin()
+        public (string Cognito, string LastLogin) CognitoAndLogin()
         {
           RegistryKey key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Kukouri\Pixel Worlds");
           string[] subKeyNames = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Kukouri\Pixel Worlds").GetSubKeyNames();
           string[] subKeysValues = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Kukouri\Pixel Worlds").GetValueNames();
           string getCognitoValue = String.Empty;
           string getLoginValue = String.Empty;
-          string getLoggedOut = String.Empty;
             foreach (var item in subKeysValues)
           {
            if (item.Contains("Cognito"))
               { getCognitoValue = item; };
            if (item.Contains("lastLoginKey"))
               { getLoginValue = item; };
-           if (item.Contains("loggedOut"))
-              { getLoggedOut = item; }
           }
           byte[] dataLogin = (byte[])key.GetValue(getLoginValue);
           byte[] dataCognito = (byte[])key.GetValue(getCognitoValue);
-          var dataLoggedOut = key.GetValue(getLoggedOut);
           string lastLoginKey = Encoding.ASCII.GetString(dataLogin.Select(x => (byte)x).ToArray());
           string cognitoIdentity = Encoding.ASCII.GetString(dataCognito.Select(x => (byte)x).ToArray());
-          return (cognitoIdentity, lastLoginKey, dataLoggedOut);
+          return (cognitoIdentity, lastLoginKey);
         }
     }
 }
